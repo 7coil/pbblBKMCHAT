b359b23bf64a93ddc71f57e4edde9548cbf177cf
7coil <leondrolio@gmail.com>
2017-01-24 21:37:16 +0000

    Added more replies
    Also added a message to say how some characters don't appear in Pebble overall

--- src/js/app.js
+++ src/js/app.js
@@ -54,7 +54,7 @@ main.on('click', 'up', function(e) {
 		sections: [{
 			items: [{
 				title: 'Hello',
-				subtitle: 'Subtitle Text'
+				subtitle: ''
 			}, {
 				title: 'Yes',
 			}, {
@@ -62,6 +62,15 @@ main.on('click', 'up', function(e) {
 			}, {
 				title: 'Navy Seal',
 				subtitle: 'What the fuck did you just'
+			}, {
+				title: 'Shrug',
+				subtitle: 'Pebble can\'t display it'
+			}, {
+				title: 'Flip',
+				subtitle: 'Pebble can\'t display it'
+			}, {
+				title: 'Unflip',
+				subtitle: 'Pebble can\'t display it'
 			}]
 		}]
 	});
@@ -81,8 +90,17 @@ main.on('click', 'up', function(e) {
 			case 3:
 				send(user, "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.", false);
 				break;
+			case 4:
+				send(user, "¯\\_(ツ)_/¯", false);
+				break;
+			case 5:
+				send(user, "(╯°□°）╯︵ ┻━┻", false);
+				break;
+			case 6:
+				send(user, "┬─┬ ノ( ゜-゜ノ)", false);
+				break;
 		}
-		
+
 		menu.hide();
 	});
 	menu.show();
